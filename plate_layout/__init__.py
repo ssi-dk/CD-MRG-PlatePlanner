df37e660a321c82dc60230be19ff3df5758ad18a
Filip (Persson) Ljung <filip.persson@gmail.com>
2023-03-03 12:43:47 +0100

    modified:   .gitignore 	modified:   plate_layout/__init__.py 	new file:   tutorial_plate_layout_v2.ipynb

--- plate_layout/__init__.py
+++ plate_layout/__init__.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: MIT
 from .plate_layout import Plate
-from .plate_layout import QCplate
+from .plate_layout import QCPlate
 from .plate_layout import Study
 
 from .logger import logger
