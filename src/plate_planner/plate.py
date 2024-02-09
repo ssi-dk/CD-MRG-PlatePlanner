a216ab20bb6f6c3d6dbcb27c255f1f0c4a291fde
filippersson <filip.persson@gmail.com>
2024-02-09 19:11:18 +0100

    Fix IndexError in as_plotly_figure by Correcting Xgrid and Ygrid Access
    Resolved an IndexError occurring in the as_plotly_figure method due to improper indexing of Xgrid and Ygrid with tuple coordinates. Previously, accessing these arrays with well.coordinate as a tuple led to retrieving entire rows or columns, instead of single values, resulting in out-of-bounds errors. This update adjusts the indexing method to explicitly use individual row and column indices from well.coordinate tuples, ensuring correct access to single values in Xgrid and Ygrid arrays. This fix stabilizes the annotation addition process in the plot generation, enhancing the function's reliability and accuracy in representing well coordinates on the plate layout.
    
    - Explicitly separated tuple indexing for Xgrid and Ygrid to access single elements.
    - Ensured compatibility with well.coordinate tuples for accurate plot annotations.
    
    	modified:   src/plate_planner/plate.py

--- src/plate_planner/plate.py
+++ src/plate_planner/plate.py
@@ -1201,9 +1201,11 @@ class Plate:
 
         # Add annotations to each well in the plate
         for well in self:
+            w_x = well.coordinate[0]
+            w_y = well.coordinate[1]
             fig.add_annotation(
-                x=Xgrid[well.coordinate],
-                y=Ygrid[well.coordinate],
+                x=Xgrid[w_x, w_y],
+                y=Ygrid[w_x, w_y],
                 text=str(well.get_attribute_or_metadata(annotation_metadata_key)),
                 textangle= -1*text_rotation,
                 showarrow=False,
@@ -1505,51 +1507,51 @@ class SamplePlate(Plate):
             well.metadata["sample_code"] = self._default_sample_code
             well.metadata["sample_name"] = self._default_sample_name
 
-    def as_plotly_figure(
-        self,
-        annotation_metadata_key="sample_code",  # Changed default value
-        color_metadata_key="sample_code",  # Changed default value
-        fontsize=14,
-        title_str=None,
-        title_fontsize=14,
-        alpha=0.7,
-        well_size=45,  # Adjusted for Plotly marker size
-        fig_width=1000,  # Adjusted for Plotly size in pixels
-        fig_height=700,  # Adjusted for Plotly size in pixels
-        colormap_continuous="Viridis",  # Default colormap in Plotly
-        colormap_discrete="D3",  # Default colormap in Plotly
-        text_rotation=0,
-        show_grid=True,
-        theme='plotly',
-        dark_mode=False,
-        marker_shape='circle'
-    ):
-        """
-        Generates a Plotly figure that visualizes the plate and optional metadata 
+    # def as_plotly_figure(
+    #     self,
+    #     annotation_metadata_key="sample_code",  # Changed default value
+    #     color_metadata_key="sample_code",  # Changed default value
+    #     fontsize=14,
+    #     title_str=None,
+    #     title_fontsize=14,
+    #     alpha=0.7,
+    #     well_size=45,  # Adjusted for Plotly marker size
+    #     fig_width=1000,  # Adjusted for Plotly size in pixels
+    #     fig_height=700,  # Adjusted for Plotly size in pixels
+    #     colormap_continuous="Viridis",  # Default colormap in Plotly
+    #     colormap_discrete="D3",  # Default colormap in Plotly
+    #     text_rotation=0,
+    #     show_grid=True,
+    #     theme='plotly',
+    #     dark_mode=False,
+    #     marker_shape='circle'
+    # ):
+    #     """
+    #     Generates a Plotly figure that visualizes the plate and optional metadata 
 
-        This method overrides the as_plotly_figure() from the Plate class to provide other defaults for annotaion and color based on QC and sample codes
-        """
-        # Call the superclass method with possibly modified default values
-        # Here, if annotation_metadata_key or color_metadata_key are not provided in the call,
-        # it uses the new defaults specified above
-        return super().as_plotly_figure(
-            annotation_metadata_key=annotation_metadata_key,
-            color_metadata_key=color_metadata_key,
-            fontsize=fontsize,
-            title_str=title_str,
-            title_fontsize=title_fontsize,
-            alpha=alpha,
-            well_size=well_size,
-            fig_width=fig_width,
-            fig_height=fig_height,
-            colormap_continuous=colormap_continuous,
-            colormap_discrete=colormap_discrete,
-            text_rotation=text_rotation,
-            show_grid=show_grid,
-            theme=theme,
-            dark_mode=dark_mode,
-            marker_shape=marker_shape
-        )
+    #     This method overrides the as_plotly_figure() from the Plate class to provide other defaults for annotaion and color based on QC and sample codes
+    #     """
+    #     # Call the superclass method with possibly modified default values
+    #     # Here, if annotation_metadata_key or color_metadata_key are not provided in the call,
+    #     # it uses the new defaults specified above
+    #     return super().as_plotly_figure(
+    #         annotation_metadata_key=annotation_metadata_key,
+    #         color_metadata_key=color_metadata_key,
+    #         fontsize=fontsize,
+    #         title_str=title_str,
+    #         title_fontsize=title_fontsize,
+    #         alpha=alpha,
+    #         well_size=well_size,
+    #         fig_width=fig_width,
+    #         fig_height=fig_height,
+    #         colormap_continuous=colormap_continuous,
+    #         colormap_discrete=colormap_discrete,
+    #         text_rotation=text_rotation,
+    #         show_grid=show_grid,
+    #         theme=theme,
+    #         dark_mode=dark_mode,
+    #         marker_shape=marker_shape
+    #     )
 
     
 # A plate with QC samples is a subclass of a Plate class
@@ -1739,51 +1741,51 @@ class QCPlate(Plate):
         self.wells[well_index].metadata["sample_type"] = self._non_qc_sample_name
         self.wells[well_index].metadata["sample_name"] = f"{self._non_qc_sample_code}{count + 1}"
 
-    def as_plotly_figure(
-        self,
-        annotation_metadata_key="sample_code",  # Changed default value
-        color_metadata_key="sample_code",  # Changed default value
-        fontsize=14,
-        title_str=None,
-        title_fontsize=14,
-        alpha=0.7,
-        well_size=45,  # Adjusted for Plotly marker size
-        fig_width=1000,  # Adjusted for Plotly size in pixels
-        fig_height=700,  # Adjusted for Plotly size in pixels
-        colormap_continuous="Viridis",  # Default colormap in Plotly
-        colormap_discrete="D3",  # Default colormap in Plotly
-        text_rotation=0,
-        show_grid=True,
-        theme='plotly',
-        dark_mode=False,
-        marker_shape='circle'
-    ):
-        """
-        Generates a Plotly figure that visualizes the plate and optional metadata 
+    # def as_plotly_figure(
+    #     self,
+    #     annotation_metadata_key="sample_code",  # Changed default value
+    #     color_metadata_key="sample_code",  # Changed default value
+    #     fontsize=14,
+    #     title_str=None,
+    #     title_fontsize=14,
+    #     alpha=0.7,
+    #     well_size=45,  # Adjusted for Plotly marker size
+    #     fig_width=1000,  # Adjusted for Plotly size in pixels
+    #     fig_height=700,  # Adjusted for Plotly size in pixels
+    #     colormap_continuous="Viridis",  # Default colormap in Plotly
+    #     colormap_discrete="D3",  # Default colormap in Plotly
+    #     text_rotation=0,
+    #     show_grid=True,
+    #     theme='plotly',
+    #     dark_mode=False,
+    #     marker_shape='circle'
+    # ):
+    #     """
+    #     Generates a Plotly figure that visualizes the plate and optional metadata 
 
-        This method overrides the as_plotly_figure() from the Plate class to provide other defaults for annotaion and color based on QC and sample codes
-        """
-        # Call the superclass method with possibly modified default values
-        # Here, if annotation_metadata_key or color_metadata_key are not provided in the call,
-        # it uses the new defaults specified above
-        return super().as_plotly_figure(
-            annotation_metadata_key=annotation_metadata_key,
-            color_metadata_key=color_metadata_key,
-            fontsize=fontsize,
-            title_str=title_str,
-            title_fontsize=title_fontsize,
-            alpha=alpha,
-            well_size=well_size,
-            fig_width=fig_width,
-            fig_height=fig_height,
-            colormap_continuous=colormap_continuous,
-            colormap_discrete=colormap_discrete,
-            text_rotation=text_rotation,
-            show_grid=show_grid,
-            theme=theme,
-            dark_mode=dark_mode,
-            marker_shape=marker_shape
-        )
+    #     This method overrides the as_plotly_figure() from the Plate class to provide other defaults for annotaion and color based on QC and sample codes
+    #     """
+    #     # Call the superclass method with possibly modified default values
+    #     # Here, if annotation_metadata_key or color_metadata_key are not provided in the call,
+    #     # it uses the new defaults specified above
+    #     return super().as_plotly_figure(
+    #         annotation_metadata_key=annotation_metadata_key,
+    #         color_metadata_key=color_metadata_key,
+    #         fontsize=fontsize,
+    #         title_str=title_str,
+    #         title_fontsize=title_fontsize,
+    #         alpha=alpha,
+    #         well_size=well_size,
+    #         fig_width=fig_width,
+    #         fig_height=fig_height,
+    #         colormap_continuous=colormap_continuous,
+    #         colormap_discrete=colormap_discrete,
+    #         text_rotation=text_rotation,
+    #         show_grid=show_grid,
+    #         theme=theme,
+    #         dark_mode=dark_mode,
+    #         marker_shape=marker_shape
+    #     )
 
     @property
     def capacity(self):
